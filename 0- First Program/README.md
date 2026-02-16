fca16f5de43183fbe006d610e3f9fee45569481e
zenos-dev <zenosfullstack@gmail.com>
2026-02-16 02:40:38 +0100

    Ajout du premier programme : main.c et README.md

---
+++ 0- First Program/README.md
@@ -0,0 +1,13 @@
+# 👋 First Program – Hello World en C
+
+![Language](https://img.shields.io/badge/Language-C-blue)
+![Compiler](https://img.shields.io/badge/Compiler-GCC-success)
+![Level](https://img.shields.io/badge/Level-Beginner-lightgrey)
+## 📌 Description
+
+This project is in line with my **first program in C language**.  
+This is a **Hello World**, designed to verify:
+
+- Proper installation of the **GCC** compiler.
+- Proper functioning of the development environment.
+- Understanding the minimal structure of a C program.
---
+++ 0- First Program/main.c
@@ -0,0 +1,20 @@
+// Inclut la bibliothèque standard pour les opérations d'entrée/sortie (printf/scanf)
+#include <stdio.h>
+
+// Fonction principale du programme - point d'entrée
+// void = ne prend aucun paramètre
+// int = retourne un entier (code de sortie du programme)
+int main(void) {
+
+    // Affiche la première ligne de texte avec un saut de ligne (\n)
+    printf("Hello, I'm Zenos-Dev.\n");
+    
+    // Affiche la deuxième ligne de texte avec un saut de ligne
+    printf("I'm a beginner in the world of software architecture.\n");
+    
+    // Affiche la troisième ligne de texte avec un saut de ligne
+    printf("And I am very happy and enthusiastic to learn and grow in this field!\n");
+
+    // Retourne 0 pour indiquer que le programme s'est exécuté avec succès
+    return 0;
+}
